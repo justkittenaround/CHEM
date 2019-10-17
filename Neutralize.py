c14a05b0a1329e697ad33c3be8c0e25f98e8d094
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-10-17 10:32:47 -0400

    Neutralization Reactions

---
+++ Neutralize.py
@@ -0,0 +1,62 @@
+from RD_load import smiles_data_loader
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import numpy as np
+
+
+tsv_path = '/home/ras/Desktop/Rachel/CHEM/hiv1_protease.tsv'
+
+smiles_data = smiles_data_loader(tsv_path)
+
+
+def _InitialiseNeutralisationReactions():
+    patts= (
+        # Imidazoles
+        ('[n+;H]','n'),
+        # Amines
+        ('[N+;!H0]','N'),
+        # Carboxylic acids and alcohols
+        ('[$([O-]);!$([O-][#7])]','O'),
+        # Thiols
+        ('[S-;X1]','S'),
+        # Sulfonamides
+        ('[$([N-;X2]S(=O)=O)]','N'),
+        # Enamines
+        ('[$([N-;X2][C,N]=C)]','N'),
+        # Tetrazoles
+        ('[n-]','[nH]'),
+        # Sulfoxides
+        ('[$([S-]=O)]','S'),
+        # Amides
+        ('[$([N-]C=O)]','N'),
+        )
+    return [(Chem.MolFromSmarts(x),Chem.MolFromSmiles(y,False)) for x,y in patts]
+
+_reactions=None
+def NeutraliseCharges(smiles, reactions=None):
+    global _reactions
+    if reactions is None:
+        if _reactions is None:
+            _reactions=_InitialiseNeutralisationReactions()
+        reactions=_reactions
+    mol = Chem.MolFromSmiles(smiles)
+    replaced = False
+    for i,(reactant, product) in enumerate(reactions):
+        while mol.HasSubstructMatch(reactant):
+            replaced = True
+            rms = AllChem.ReplaceSubstructs(mol, reactant, product)
+            mol = rms[0]
+    if replaced:
+        return (Chem.MolToSmiles(mol,True), True)
+    else:
+        return (smiles, False)
+
+neutralized_bin = []
+smiles_bin = []
+for idx, smile in enumerate(smiles_data):
+    (molSmiles, neutralized) = NeutraliseCharges(smile)
+    if neutralized:
+        neutralized_bin.append(molSmiles)
+        smiles_bin.append(smile)
+
+neutralized_rxns = np.asarray(zip(smiles_bin, neutralized_bin))
