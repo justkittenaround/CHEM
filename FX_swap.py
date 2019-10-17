c58364a40164b0e8bf74e5c112adb41684a9fe36
Rachel A St Clair <rachel.aileen.st@gmail.com>
2019-10-17 14:16:35 -0400

    Randomly Replace Functional Groups
    Save as TSV

--- FX_swap.py
+++ FX_swap.py
@@ -10,14 +10,13 @@ smiles_data = smiles_data_loader(tsv_path)
 
 
 def _initialize_groups():
-    fx_groups = np.asarray(['C=O', '[H+]', '[CX3]=[OX1]'])
+    fx_groups = np.asarray(['C=O', '[H+]', '[CX3]=[OX1]', '[CX3](=[OX1])C', '[OX1]=CN', '[CX3](=[OX1])O', '[CX3](=[OX1])[F,Cl,Br,I]', '[CX3H1](=O)[#6]', '[CX3](=[OX1])[OX2][CX3](=[OX1])', '[NX3][CX3](=[OX1])[#6]', '[NX3][CX3]=[NX3+]', '[NX3,NX4+][CX3](=[OX1])[OX2,OX1-]',  '[CX3](=O)[OX2H1]', '[NX3][CX2]#[NX1]', '[#6][CX3](=O)[#6]', '[OD2]([#6])[#6]', '[NX3;H2,H1;!$(NC=O)]', '[NX3,NX4+][CX4H]([*])[CX3](=[OX1])[O,N]', '[$(*-[NX2-]-[NX2+]#[NX1]),$(*-[NX2]=[NX2+]=[NX1-])]', '[NX2]=N', '[NX3][NX3]', '[NX3][NX2]=[*]', '[CX3;$([C]([#6])[#6]),$([CH][#6])]=[NX2][#6]', '[NX3+]=[CX3]', '[CX3](=[OX1])[NX3H][CX3](=[OX1])', '[$([NX3](=[OX1])(=[OX1])O),$([NX3+]([OX1-])(=[OX1])O)]', '[NX1]#[CX2]', '[$([NX3](=O)=O),$([NX3+](=O)[O-])][!#8]', '[NX2]=[OX1]', '[$([#7+][OX1-]),$([#7v5]=[OX1]);!$([#7](~[O])~[O]);!$([#7]=[#7])]', '[OX2H]', '[$([OH]-*=[!#6])]', '[OX2,OX1-][OX2,OX1-]', '[$(P(=[OX1])([$([OX2H]),$([OX1-]),$([OX2]P)])([$([OX2H]),$([OX1-]),$([OX2]P)])[$([OX2H]),$([OX1-]),$([OX2]P)]),$([P+]([OX1-])([$([OX2H]),$([OX1-]),$([OX2]P)])([$([OX2H]),$([OX1-]),$([OX2]P)])[$([OX2H]),$([OX1-]),$([OX2]P)])]', '[#16X2H]', '[#16X2H0]', '[$([#16X3](=[OX1])[OX2H0]),$([#16X3+]([OX1-])[OX2H0])]', '[$([#16X4](=[OX1])=[OX1]),$([#16X4+2]([OX1-])[OX1-])]', '[$([#16X3]=[OX1]),$([#16X3+][OX1-])]', '[$([#16X4](=[OX1])(=[OX1])([OX2H,OX1H0-])[OX2][#6]),$([#16X4+2]([OX1-])([OX1-])([OX2H,OX1H0-])[OX2][#6])]', '[$([#16X4]([NX3])(=[OX1])(=[OX1])[OX2][#6]),$([#16X4+2]([NX3])([OX1-])([OX1-])[OX2][#6])]', '[#16X2][OX2H,OX1H0-]', '[#16X2][OX2H0]', '[CX3](=[OX1])[F,Cl,Br,I]'])
     return fx_groups
 
 
 _groups = None
 def swap_fx(smiles_data):
-
-    _groups = initialize_groups()
+    _groups = _initialize_groups()
     groups = _groups
     swapped_bin = []
     smiles_bin = []
@@ -46,9 +45,9 @@ def swap_fx(smiles_data):
             smiles_bin.append(m)
             print('swapped')
     swap_rxns = np.asarray(zip(smiles_bin, swapped_bin))
-    with open('deletion_reactions.tsv', 'wt') as out_file:
+    with open('swap-fx_reactions.tsv', 'wt') as out_file:
         tsv_writer = csv.writer(out_file, delimiter='\t')
-        for thing in deletion_rxns:
+        for thing in swap_rxns:
             thingone = thing[0]
             thingtwo = thing[1]
             tsv_writer.writerow([thingone, thingtwo])
